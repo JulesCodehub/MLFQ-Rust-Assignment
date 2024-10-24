a418318f0840ad4a29dd6a98ae3e04de6c52f84b
JulesCodehub <180057196+JulesCodehub@users.noreply.github.com>
2024-10-24 01:32:36 +0000

    fixed multiple errors and typos

--- mlfq_scheduler/src/mlfq.rs
+++ mlfq_scheduler/src/mlfq.rs
@@ -37,8 +37,8 @@ impl MLFQ {
         let prio = process.priority; //make sure this value is immutable
 
         match prio {
-            1..=num_levels => queues[prio-1].add(process);
-            _ => queues[0].add(process); // 2. Edge case: if bounds is outside of range 1...num_levels, we need to put it in the 0th queue
+            1..self.num_levels => queues[prio-1].add(process),
+            _ => self.queues[0].add(process), // 2. Edge case: if bounds is outside of range 1...num_levels, we need to put it in the 0th queue
         }
     }
 
@@ -46,20 +46,20 @@ impl MLFQ {
     pub fn execute_process(&mut self, queue_index: usize) {
 
         // 1. given queue_index, determine which level and actual index in the queues this process falls in
-        let (level, idx) = find_index(self, queue_index);
+        let (level, idx, err) = self.find_index(queue_index);
 
         // 2. Edge case: if this index does not exist, we need to return as there is nothing to process
-        if level == -1 {
+        if err {
             return
         }
 
         // 3. using this proper index lets copy the information to preform some math on it locally
-        let proc_time = queue[level][idx].remaining_time;
-        let t_q = time_quanta[level];
+        let proc_time = self.queues[level][idx].remaining_time;
+        let t_q = self.time_quanta[level];
 
         // 4. determine how long the process runs for
-        mut time_ran: i32;
-        mut job_left: i32;
+        let mut time_ran: u32;
+        let mut job_left: u32;
 
         if proc_time == 0 {
             time_ran = 0;
@@ -75,38 +75,38 @@ impl MLFQ {
         }
 
         // 5. update the process information based on the math
-        queue[level][idx].remaining_time = job_left;
-        queue[level][idx].total_executed_time += time_ran;
+        self.queues[level][idx].remaining_time = job_left;
+        self.queues[level][idx].total_executed_time += time_ran;
 
         // 6. if the process does not have anything left to run, eject it
         if job_left == 0 {
-            queue[level][idx].remove();
+            self.queues[level][idx].remove();
         }
 
         // 7. if instead the process ran all of its time quanta, we need to move it to the next lower queue
         // 8. Edge case: if the process is already in the lowest queue, we dont need to do anything
         // We will use this as a condition to ignore execution for this whole block
-        if time_ran >= t_q && level < num_levels {
-            queue[level][idx].priority += 1;
-            queue[num_levels].add(queue[level][idx].remove());
+        if time_ran >= t_q && level < self.num_levels {
+            self.queues[level][idx].priority += 1;
+            self.queues[self.num_levels].add(self.queues[level][idx].remove());
         }
 
         // 9. update the global time
-        current_time += time_ran;
+        self.current_time += time_ran;
     }
 
     // Exercise 3: Priority Boost
     pub fn priority_boost(&mut self) {
 
         // 1. Move any processes not in the 0th level to the 0th level in the most elegant way possible
-        for i in 1..nums_levels {
-            while !queue[i].isempty() {
-                queue[0].add(queue[i][0].remove()));
+        for i in 1..self.num_levels {
+            while !self.queues[i].is_empty() {
+                self.queues[0].add(self.queues[i][0].remove());
             }
         }
 
         // 2. Assign every process to have 0 priority
-        for p in queues[0] {
+        for p in self.queues[0] {
             p.priority = 0;
         }
     }
@@ -121,24 +121,25 @@ impl MLFQ {
     }
 
     // Helper function to convert queue_index to real index
-    pub fn find_index(&mut self, queue_index: usize) -> (usize, usize) {
+    pub fn find_index(&mut self, queue_index: usize) -> (usize, usize, bool) {
 
         let mut level = 0;
         let mut idx = queue_index;
+        let mut err = false;
 
-        for i in 0...num_levels {
+        for i in 0..self.num_levels {
             let len = nums[i].len();
             let c = idx % len;
                 if c == idx {
                     idx = c;
+                    err = true;
                     break;
                 }
                 idx -= len;
                 level += 1;
-            }
         }
 
-        (level, idx)
+        (level, idx, err)
     }
 }
 
